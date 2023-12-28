a8fa7eb5cfa08906a85df5bcd42b9d484a5d20ff
KieuSonTung <sontungkieu412@gmail.com>
2023-12-28 16:24:11 +0700

    Fix data visualization

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import taipy as tp
 from taipy.gui import Gui, Icon, navigate
 from src.config.config import scenario_cfg
 from taipy.config import Config 
+from src.pages.data_visualization import *
+from src.pages.model_management import *
+
 
 # Load configuration
 Config.load('src/config/config.toml')
@@ -12,17 +15,48 @@ scenario_cfg = Config.scenarios['customer_segmentation']
 # Execute the scenario
 tp.Core().run()
 
+# Function that is called when there is a change in the menu control
+def menu_fct(state, var_name, var_value):
+    state.page = var_value['args'][0]
+    navigate(state, state.page.replace(" ", "-"))
+
+# Create and submit the first scenario
 def create_first_scenario(scenario_cfg):
-    """Create and submit the first scenario."""
     scenario = tp.create_scenario(scenario_cfg)
     tp.submit(scenario)
     return scenario
 
 scenario = create_first_scenario(scenario_cfg)
 
+# read dataset
 ds = scenario.preprocessed_dataset.read()
 
+# run model
 results = scenario.trained_model.read()
 pred = results['Clusters']
 
-print(results)
+# visualization
+heatmap_dataset = creation_heatmap_dataset(ds)
+
+# model management
+
+
+root_md = """
+<|toggle|theme|>
+<|menu|label=Menu|lov={menu_lov}|on_action=menu_fct|>
+"""
+
+menu_lov = [
+    ("Data Visualization", Icon('src/images/histogram_menu.svg', 'Data Visualization'))
+]
+
+# Define pages
+pages = {
+    "/": root_md,
+    "Data-Visualization": dv_data_visualization_md
+}
+
+# Run the GUI
+if __name__ == '__main__':
+    gui = Gui(pages=pages)
+    gui.run(title="Customer Segmetation", dark_mode=False, port=8866)
--- src/pages/data_visualization.py
+++ src/pages/data_visualization.py
@@ -10,17 +10,49 @@ pallet = ["#682F2F", "#9E726F", "#D6B2B1", "#B9C0C9", "#9F8A78", "#F3AB60"]
 cmap = colors.ListedColormap(["#682F2F", "#9E726F", "#D6B2B1", "#B9C0C9", "#9F8A78", "#F3AB60"])
 
 
-def relative_plot(ds: pd.DataFrame):
+def creation_scatter_dataset(ds: pd.DataFrame):
     To_Plot = [ "Income", "Recency", "Customer_For", "Age", "Spent", "Is_Parent"]
-    plt.title("Reletive Plot Of Some Selected Features: A Data Subset")
-    plt.figure()
-    sns.pairplot(ds[To_Plot], hue= "Is_Parent",palette= (["#682F2F","#F3AB60"]))
+    scatter_dataset = ds[To_Plot]
+    
+    return scatter_dataset
 
-    plt.show()
+def creation_heatmap_dataset(ds: pd.DataFrame):
+    corr_df= ds.corr()
+    corrmat = {
+        'Columns': corr_df.columns.tolist(),
+        'Index': corr_df.index.tolist(),
+        'Values': corr_df.values.tolist()
+    }
+    
+    return corrmat
 
-def correlation_matrix(ds: pd.DataFrame):
-    #correlation matrix
-    corrmat= ds.corr()
-    plt.figure(figsize=(20,20))  
-    sns.heatmap(corrmat, annot=True, cmap=cmap, center=0)
+def update_plot(state):
+    # global x_selected, y_selected
+    # x_selected = state.x_selected
+    # y_selected = state.y_selected
+    # state.properties_scatter_dataset =  {"x":x_selected,
+    #                                      "y":y_selected}
+    state.corrmat = state.corrmat
+
+
+dv_graph_selector = ['Relative', 'Heatmap']
+dv_graph_selected = dv_graph_selector[0]
+
+dv_data_visualization_md = """
+# Data **Visualization**{: .color-primary}
+<|{dv_graph_selected}|toggle|lov={dv_graph_selector}|>
+
+--------------------------------------------------------------------
+
+<|part|render={dv_graph_selected == 'Scatter'}|
+### Scatter
+|>
+
+<|part|render={dv_graph_selected == 'Heatmap'}|
+### Heatmap
+
+<|{heatmap_dataset}|chart|type=heatmap|z=Values|x=Columns|y=Index|height=1000px|width=1700px|>
+|>
+
+"""
 
