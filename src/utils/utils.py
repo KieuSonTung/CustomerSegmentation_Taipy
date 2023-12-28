f75943938040ab027d39379698b9b4d9a6738bcc
KieuSonTung <sontungkieu412@gmail.com>
2023-12-28 17:24:40 +0700

    Fix bug DATA_DIR

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,6 +6,6 @@ class AppPath:
     UTILS_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
     SRC_DIR = UTILS_DIR.parent
     ROOT_DIR = SRC_DIR.parent
-    DATA_DIR = Path(ROOT_DIR, "data")
+    DATA_DIR = Path(SRC_DIR, "data")
 
     DATA_FILE_PATH = Path(DATA_DIR, "data.csv")
