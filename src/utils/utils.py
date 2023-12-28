cd243e0f945aa3636ba584958a79c87963d1cddd
KieuSonTung <sontungkieu412@gmail.com>
2023-12-28 16:58:04 +0700

    Add folder utils

---
+++ src/utils/utils.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+
+class AppPath:
+    UTILS_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+    SRC_DIR = UTILS_DIR.parent
+    ROOT_DIR = SRC_DIR.parent
+    DATA_DIR = Path(ROOT_DIR, "data")
+
+    DATA_FILE_PATH = Path(DATA_DIR, "data.csv")
